5ec3412d43943a702354df73552eb2d222d254fc
chloe-chin <57428654+chloe-chin@users.noreply.github.com>
2020-04-16 22:48:48 -0700

    portfolio - original version

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Chloe Chin, Period One]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Chin_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Chin_Resume.html">Resume.html</a></li></br>
+      <li><a href="Chin_Projects.html">Projects.html</a></li></br>
+      <li><a href="Chin_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
